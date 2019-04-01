cfb9e850f68df76489fe021a21b07c07ed213862
Javier <Jvictor@webimpacto.es>
2019-04-01 09:36:59 +0200

    WEB-1669 ultimo commit arreglo7

--- wim_objectlogguer.php
+++ wim_objectlogguer.php
@@ -37,7 +37,7 @@ class WimObjectLogguer extends Module
         $this->name = 'wim_objectlogguer';
         $this->tab = 'front_office_features';
         $this->version = '1.0.0';
-        $this->author = 'Carlos';
+        $this->author = 'Javier';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
         $this->bootstrap = true;
